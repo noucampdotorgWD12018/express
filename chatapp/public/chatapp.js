dd19a49668770df3606c6c4532d66ee020ff1e61
barcaxi <thomas.devine@lyit.ie>
2018-10-22 15:31:23 +0100

    'init'

---
+++ chatapp/public/chatapp.js
@@ -0,0 +1,8 @@
+$("document").ready(function() {
+
+	// Initially just add the messages to the div when the Send button is pressed
+	
+	// Later post the message to the chatapp server
+	// and get all the message and add to the div
+
+});
