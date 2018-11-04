c7352cd17fab8747c4f525cb48e053b0f4b79cc1
Thomas Devine <tomo@thomass-imac.home>
2018-11-04 23:59:23 +0000

    init

---
+++ restapi1/routes/routes.js
@@ -0,0 +1,30 @@
+exports.routes = function(app){
+
+    // a chainable route handler for the route path '/team' using Express route() function 
+    app.route('/team')
+      .get(function (req, res) {
+        res.send('GET: a team!')
+      })
+      .post(function (req, res) {
+        res.send('POST: a team!')
+      })
+      .put(function (req, res) {
+        res.send('PUT: a team!')
+      })
+      .delete(function (req, res) {
+        res.send('DELETE: a team!')
+      });
+
+    // specific route handler for path '/team/id' for GET, PUT & DELETE
+    app.route('/team/:id')
+      .get(function (req, res) {
+        res.send(`GET: team ${req.params.id}`);
+      })
+      .put(function (req, res) {
+        res.send(`PUT: team ${req.params.id}`);
+      })
+      .delete(function (req, res) {
+        res.send(`DELETE: team ${req.params.id}`);
+      });
+
+}
